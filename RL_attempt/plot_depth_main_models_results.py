27fbc7955520817c5b1f614c5111754c6e753da5
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2024-01-18 02:35:55 +0800

    change figure titles

--- RL_attempt/plot_depth_main_models_results.py
+++ RL_attempt/plot_depth_main_models_results.py
@@ -1,7 +1,7 @@
 import plot_astar_depths_results 
 
-cases = {'max_12_filtered_0_1': [100, 2, "max_12_filtered_0_1", [5e-04], [0.1], []], 
-         #'max_12': [100, 2, "max_12", [5e-04], [0.1], []], 
+cases = {#'max_12_filtered_0_1': [100, 2, "max_12_filtered_0_1", [5e-04], [0.1], []], 
+         'max_12': [100, 2, "max_12", [5e-04], [0.1], []], 
          #'max_15': [100, 2, "max_15", [5e-05], [0.1], []], 
          #'max_12_filtered_0_1': [100, 3, "max_12_filtered_0_1", [5e-04], [0.1], []], 
          #'max_12': [100, 3, "max_12", [5e-04], [0.1], []], 
